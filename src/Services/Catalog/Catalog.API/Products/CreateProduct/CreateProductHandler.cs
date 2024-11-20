a13ed712f9137bdb9d89ebe1d11f80d61bb9b097
Oliwier Baran <oliwier.baran@icloud.com>
2024-11-20 12:01:03 +0100

    initial catalog model with cqrs pattern

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, decimal Price)
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
