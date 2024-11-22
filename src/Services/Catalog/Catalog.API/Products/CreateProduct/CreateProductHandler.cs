c8accc842a09202030e11a5204da3adbaf9c4c75
Oliwier Baran <oliwier.baran@icloud.com>
2024-11-22 15:24:27 +0100

    centralise exception handler to validation behavior with fix

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -6,18 +6,23 @@ namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
-
 public record CreateProductResult(Guid Id);
 
 public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
 {
     public CreateProductCommandValidator()
     {
-        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
-        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
-        RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
-        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required.");
-        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than zero.");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .Length(2,150).WithMessage("Name must be between 2 and 150 characters.");
+        RuleFor(x => x.Category)
+            .NotEmpty().WithMessage("Category is required.");
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.");
+        RuleFor(x => x.ImageFile)
+            .NotEmpty().WithMessage("ImageFile is required.");
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than zero.");
     }
 }
 
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -4,6 +4,25 @@ public record UpdateProductCommand(Guid Id, string Name, List<string> Category, 
     : ICommand<UpdateProductResult>;
 public record UpdateProductResult(bool IsSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.");
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.");
+        RuleFor(x => x.Category)
+            .NotEmpty().WithMessage("Category is required.");
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.");
+        RuleFor(x => x.ImageFile)
+            .NotEmpty().WithMessage("ImageFile is required.");
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than zero.");
+    }
+}
+
 internal class UpdateProductCommandHandler
     (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
