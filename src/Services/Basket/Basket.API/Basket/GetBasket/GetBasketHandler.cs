bdfbd4aa8f0f4a5d16a13b7de021282937e54bac
Oliwier Baran <oliwier.baran@icloud.com>
2024-11-25 15:45:31 +0100

    implement get basket endpoint

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,11 +1,8 @@
-
-
 namespace Basket.API.Basket.GetBasket;
 
-
 public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
-
 public record GetBasketResult(ShoppingCart Cart);
+
 public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
     public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,2 +1,6 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
+
 global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
