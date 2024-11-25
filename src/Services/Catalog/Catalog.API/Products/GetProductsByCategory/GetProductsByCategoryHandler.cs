d8354ad8282f8fde300982d9943c24e91a307f1a
Oliwier Baran <oliwier.baran@icloud.com>
2024-11-25 10:35:20 +0100

    clean up old logging method

--- src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -4,13 +4,11 @@ public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsBy
 public record GetProductsByCategoryResult(IEnumerable<Product> Products);
     
 internal class GetProductsByCategoryQueryHandler 
-    (IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger)
+    (IDocumentSession session)
     : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
 {
     public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@Query}", query);
-
         var products = await session.Query<Product>()
             .Where(p => p.Category.Contains(query.Category))
             .ToListAsync(cancellationToken);
