eff388f3907386f6ed0b55f989ed5ae59429a697
Oliwier Baran <oliwier.baran@icloud.com>
2024-11-26 11:23:14 +0100

    implement repository and add exception

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,8 +1,11 @@
 global using Carter;
 global using FluentValidation;
-global using Mapster;
 global using MediatR;
+global using Mapster;
+global using Marten;
 
+global using Basket.API.Exceptions;
 global using Basket.API.Models;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
