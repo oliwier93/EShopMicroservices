c4a93e8c07a584a2fbd51e05b900a39f4a43d88e
Oliwier Baran <oliwier.baran@icloud.com>
2024-11-28 10:12:42 +0100

    seed discount db

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DiscountContext>(opts =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 app.MapGet("/",
     () =>
