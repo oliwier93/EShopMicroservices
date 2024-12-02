1951ecd714ade98f13bfff44d15baf796b576850
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-02 09:41:13 +0100

    develop ddd for address and payment

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -2,5 +2,17 @@ namespace Ordering.Domain.ValueObjects;
 
 public record OrderId
 {
-    public Guid value { get; }
+    public Guid Value { get; }
+    private OrderId(Guid value) => Value = value;
+
+    public static OrderId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty)
+        {
+            throw new DomainException("CustomerId cannot be empty.");
+        }
+
+        return new OrderId(value);
+    }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -7,4 +7,28 @@ public record Payment
     public string Expiration { get; } = default!;
     public string CVV { get; } = default!;
     public int PaymentMethod { get; } = default!;
+
+    protected Payment()
+    {
+        
+    }
+
+    private Payment(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+    {
+        CardName = cardName;
+        CardNumber = cardNumber;
+        Expiration = expiration;
+        CVV = cvv;
+        PaymentMethod = paymentMethod;
+    }
+
+    public static Payment Create(string cardName, string cardNumber, string expiration, string cvv, int paymentMethod)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardName);
+        ArgumentNullException.ThrowIfNull(cardNumber);
+        ArgumentNullException.ThrowIfNull(cvv);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(cvv.Length, 3);
+        
+        return new Payment(cardName, cardNumber, expiration, cvv, paymentMethod);
+    }
 }
