131802aea61ed1174f6d1b1e7c8a04f7e8e29d25
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-02 10:27:21 +0100

    implement ddd for order entities

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -2,8 +2,8 @@ namespace Ordering.Domain.Models;
 
 public class Customer : Entity<CustomerId>
 {
-    public string Name { get; set; } = default!;
-    public string Email { get; set; } = default!;
+    public string Name { get; private set; } = default!;
+    public string Email { get; private set; } = default!;
 
     public static Customer Create(CustomerId id, string name, string email)
     {
--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,7 +1,24 @@
+using System.Dynamic;
+
 namespace Ordering.Domain.Models;
 
 public class Product : Entity<ProductId>
 {
-    public string Name { get; set; } = default!;
-    public decimal Price { get; set; } = default!;
+    public string Name { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+    
+    public static Product Create(ProductId id, string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Price = price
+        };
+
+        return product;
+    }
 }
