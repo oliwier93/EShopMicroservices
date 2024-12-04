a789b784b4e0a0f8c35a8b16fc9562072cd3ec11
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-04 14:22:07 +0100

    develop dto files

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,9 @@
+namespace Ordering.Application.Dtos;
+
+public record AddressDto(
+    string FirstName,
+    string LastName,
+    string EmailAddress,
+    string Country,
+    string State,
+    string ZipCode);
