8fe102222563a201f32a6335873f8023a94f965a
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-04 15:08:30 +0100

    dbcontext injection in ordering

---
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+using Ordering.Domain.Models;
+
+namespace Ordering.Application.Data;
+
+public interface IApplicationDbContext
+{
+    DbSet<Customer> Customers { get; }
+    DbSet<Order> Orders { get; }
+    DbSet<OrderItem> OrderItems { get; }
+    DbSet<Product> Products { get; }
+    
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
