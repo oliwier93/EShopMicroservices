552ab74f27c12f3f5fa2207f5d1886c513f31803
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-05 13:03:50 +0100

    implement event handlers in domain

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,6 +1,10 @@
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+
 global using BuildingBlocks.CQRS;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
+global using Ordering.Domain.Events;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
