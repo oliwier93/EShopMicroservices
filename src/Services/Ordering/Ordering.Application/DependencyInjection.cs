96de66dd2c6d5ef3be55cb32c409f3477406b56a
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-05 12:33:14 +0100

    register behaviors

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using BuildingBlocks.Behaviors;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
@@ -7,8 +8,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddMediatR(cfg => {
-          cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(config => {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
         return services;
