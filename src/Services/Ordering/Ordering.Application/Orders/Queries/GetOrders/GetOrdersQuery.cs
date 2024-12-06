89cdbc342b0ebf5f7ac83d6bd79f8ba75fc0324c
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-06 14:24:35 +0100

    impl get orders with pagination

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -1,3 +1,8 @@
+using BuildingBlocks.Pagination;
+
 namespace Ordering.Application.Orders.Queries.GetOrders;
 
-public record GetOrdersQuery();
+public record GetOrdersQuery(PaginatedRequest PaginationRequest)
+    : IQuery<GetOrdersResult>;
+
+public record GetOrdersResult(PaginatedResult<OrderDto> PaginationResult);
