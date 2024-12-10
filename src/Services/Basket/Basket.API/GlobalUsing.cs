7e0a2b79fbf0acb9cd3211d5f380210b92be22a9
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-10 12:07:19 +0100

    add basket checkout feature folder

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -5,6 +5,7 @@ global using Mapster;
 global using Marten;
 
 global using Basket.API.Data;
+global using Basket.API.Dtos;
 global using Basket.API.Exceptions;
 global using Basket.API.Models;
 global using BuildingBlocks.Behaviors;
