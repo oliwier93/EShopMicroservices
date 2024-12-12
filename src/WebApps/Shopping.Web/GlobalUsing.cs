138c67b619b69891f93ab76a2c67419bd785ed89
Oliwier Baran <oliwier.baran@icloud.com>
2024-12-12 14:43:55 +0100

    add ibasket service

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -1,4 +1,6 @@
 global using Refit;
 
+global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Ordering;
 global using Shopping.Web.Services;
